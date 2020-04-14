2f61d51aa4042d3bcbcd3007a10da1d8de400e8e
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2020-04-13 23:46:34 -0400

    Update arbnumerics.jl

--- src/intervals/arbnumerics.jl
+++ src/intervals/arbnumerics.jl
@@ -1,1 +1,49 @@
+# ArbNumerics is imported in IntervalArithmetic.jl
+using ArbNumerics: ArbFloat, ArbReal, ArbComplex, 
+    setworkingprecsion, setextrabits,
+    setball, setinterval, lobound, hibound
 
+export gamma
+
+const SupplementalBits = 32
+
+mutable struct PriorPrecision
+    type::Type
+    prec::Int
+end    
+
+# initialize to something that does not occur
+const PriorPrec = PriorPrecision(UInt8, -1)  
+
+function resetworkingprecision(supplementalbits::Int=SupplementalBits)
+    typ, prec = precision(Interval)
+    if prec !== PriorPrec.prec    
+        workingprec = prec + supplementalbits
+        setextrabits(0)
+        setworkingprecision(ArbFloat, workingprec)
+        # uncomment to future-proof
+        # setworkingprecision(ArbReal, workingprec)
+        # setworkingprecision(ArbComplex, workingprec)
+        PriorPrec.type = typ
+        PriorPrec.prec = prec
+    end
+    return nothing
+end
+
+function arbreal(x::Interval)
+    resetworkingprecision()
+    xlo = x.lo
+    xhi = x.hi
+    extraprec = Prior.prec + SupplementalBits
+    lo = copy(ArbFloat(x, extraprec), Prior.prec, RoundDown)
+    hi = copy(ArbFloat(x, extraprec), Prior.prec, RoundUp)
+    return setinterval(lo, hi)
+ end
+ 
+function gamma(x::Interva{T}) where T
+    result = gamma(arbreal(x))
+    lo, hi = ArbNumerics.interval(result)
+    ylo, yhi = T(lo), T(hi)
+    return Interval(ylo, yhi)
+ end 
+    
